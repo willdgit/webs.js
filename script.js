85d6f943abb7bfffe288e9cfe47e2ef975252b0d
William Derksen <97864183+willdgit@users.noreply.github.com>
2022-01-19 01:40:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onload = function () {
     let circles = [];
     let numCircles = 0;
     let radius = 1;//0.5 look scool
-    let spawnVel = 0.5;
+    let spawnVel = 0.75;
     
     //constraint vars
     let opacityStep = 0.1;
@@ -25,24 +25,33 @@ window.onload = function () {
     var timestep = 15;
     var intervalId;
 
-    //color vars
-    var color;
     var colorR = 255;
     var colorG = 255;
     var colorB = 255;
 
-    //use session to get desired amount of points
+    //use session storage to get desired amount of points
     let amount = sessionStorage.getItem("amount");
     if (amount == null || amount == 0 || amount == "") {
         amount = 200;
     }
 
-    //return random float between max and min
-    function getRandomNum(max, min) {
+    //return random float between max and min, round to nearest int by default
+    function getRandomNum(max, min, round = 1) {
         num = Math.random() * (max - min) + min;
-        return Math.round(num * 4) / 4;;//round to nearest 0.25
+        return Math.round(num * round) / round;//round = 4 will round to nearest 0.25
     }
     
+    //hex value to seperate r, g, and b method courtesy stackoverflow user Tim Down
+    //https://stackoverflow.com/a/5624139 
+    function hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
     //TODO find out how to make mouse influence work
     //if point within radius from mouse pos, set its past vel to 0?
 
@@ -60,31 +69,32 @@ window.onload = function () {
             this.x = x;
             this.y = y;
 
-            this.pastX = x + getRandomNum(spawnVel, -spawnVel);
-            this.pastY = y + getRandomNum(spawnVel, -spawnVel);
+            this.pastX = x + getRandomNum(spawnVel, -spawnVel, 4);
+            this.pastY = y + getRandomNum(spawnVel, -spawnVel, 4);
             this.vx = 0;
             this.vy = 0;
         }
 
         update() {
             //preform the verlet, velocity is calculated based on our past and current position
-            //no need to asign velocity to a var, it will be constant (no drag / gravity)
+            //no (real) need to asign velocity to a var, it will be constant through the simulation (no drag / gravity)
             this.vx = this.x - this.pastX;
             this.vy = this.y - this.pastY;
 
-            let newX = this.x + /*(this.x - this.pastX) +*/ this.vx;
+            //update next position based on that velocity
+            let newX = this.x + this.vx;
             let newY = this.y + this.vy;
 
+            //update past and next positions
             this.pastX = this.x;
             this.pastY = this.y;
-
-            //and we update our next position based on that velocity
             this.x = newX;
             this.y = newY;
 
+            //reset velocity
             this.vy = this.vx = 0;
 
-            //screen bounce logic
+            //edge bounce logic (if point is beyond the canvas, move it to edge and reverse direction)
             if (this.x >= canvas.width) {
                 this.pastX = canvas.width + (canvas.width - this.pastX);
                 this.x = canvas.width;
@@ -102,6 +112,8 @@ window.onload = function () {
             this.constraints.forEach((constraint) => constraint.update());
         }
 
+        //we may not need to set i here, since every point is connected to every point, we 
+        //should know the exact number of constraints for each point (n factorial?)
         draw() {
             let i = this.constraints.length;
             while (i--) {
@@ -115,14 +127,17 @@ window.onload = function () {
             ctx.stroke();
             ctx.fill();
         }
+        //create a new constraint between this and point, add it to constraints[]
         attach(point) {
             this.constraints.push(new Constraint(this, point));
         }
+        //remove constraint from this points constraint array
         free(constraint) {
             this.constraints.splice(this.constraints.indexOf(constraint), 1);
         }
     }
 
+    //each constraint has 2 points
     class Constraint {
         constructor(p1, p2) {
             this.p1 = p1;
@@ -137,14 +152,14 @@ window.onload = function () {
             let dy = this.p1.y - this.p2.y;
             this.dist = Math.sqrt(dx * dx + dy * dy);
 
-            // if (this.dist > 200){
+            //this could really save performace
+            // if (this.dist > 500){
             //    this.p1.free(this);
             //    this.p2.free(this);
-            // }else{
-            //     this.p1.attach(this.p2);
+            // }else if we are not attached{
+            //     //this.p1.attach(this.p2);
             // }
-
-            //console.log(dist);
+            
             //for every int distance less than threshold (say 250 or smth(500 is too high)) increase alpha by 0.1?
             let diff = threshold - this.dist;
 
@@ -167,15 +182,17 @@ window.onload = function () {
         }
     }
 
-    var c;//inital circle
+    var c;//inital circle (declared here to be refrenced from canvas click listener)
     //spawns a cricle at a random x and y
     function spawnCircle(x, y) {
         c = new Circle(x, y);
         circles[numCircles] = c;
         numCircles++;
+        //update view
         document.getElementById("count").innerHTML = numCircles + " particles";
     }
 
+    //call spawnCircle() amount times, attaching each circle to all others
     for (let index = 0; index < amount; index++) {
         spawnCircle(getRandomNum(canvas.width - radius, radius * 2), getRandomNum(canvas.height - radius, radius * 2));
         if (index !== 0) {
@@ -187,13 +204,13 @@ window.onload = function () {
         }
     }
 
-
-    //update the canvas every 10 ms,
+    //update the canvas every _interval ms
     function startInterval(_interval) {
-        // Store the id of the interval so we can clear it later
+        //store the id of the interval so we can stop & re-start it later
         intervalId = setInterval(update, _interval);
     }
 
+    //main canvas update function
     function update() {
         //fill the canvas
         ctx.beginPath();
@@ -210,80 +227,6 @@ window.onload = function () {
         });
     }
 
-    //#region slider UI
-
-    var radSlider = document.getElementById("radius");
-    radSlider.addEventListener('input', function () {
-        radius = radSlider.value;
-    });
-
-    var opacitySlider = document.getElementById("opacity");
-    opacitySlider.addEventListener('input', function () {
-        opacityStep = opacitySlider.value;
-    });
-
-    var thresholdSlider = document.getElementById("threshold");
-    thresholdSlider.addEventListener('input', function () {
-        threshold = thresholdSlider.value;
-    });
-
-    var distThresholdSlider = document.getElementById("distThreshold");
-    distThresholdSlider.addEventListener('input', function () {
-        distThreshold = distThresholdSlider.value;
-    });
-
-
-    timestepSlider.addEventListener('mouseup', function () {
-        timestep = timestepSlider.value;
-        clearInterval(intervalId);
-        startInterval(timestep);
-    });
-
-    var amountBox = document.getElementById("amount");
-    amountBox.addEventListener('input', function () {
-        sessionStorage.setItem("amount", amountBox.value);
-    });
-
-    //pause button toggle
-    var pauseToggle = false;
-    var pause = document.getElementById("pause");
-    pause.addEventListener("click", function () {
-        if (!pauseToggle) {
-            clearInterval(intervalId);
-            pauseToggle = true;
-            pause.innerHTML = "Play";
-        } else {
-            startInterval(timestepSlider.value);
-            pauseToggle = false;
-            pause.innerHTML = "Pause";
-        }
-    });
-
-    //slider div toggle
-    var divToggle = true;
-    sliderButton.addEventListener("click", function () {
-        if (!divToggle) {
-            sliderBG.style.display = 'none';
-            sliderDiv.style.display = 'none';
-            divToggle = true;
-        }
-        else {
-            sliderBG.style.display = 'block';
-            sliderDiv.style.display = 'block';
-            divToggle = false;
-        }
-    });
-
-    //colour picker listener
-    document.getElementById("colorPicker").addEventListener("change", function (event) {
-        var color = event.target.value;
-        colorR = hexToRgb(color).r;
-        colorG = hexToRgb(color).g;
-        colorB = hexToRgb(color).b
-    }, false);
-
-    //#endregion slider UI
-
     //add a circle on canvas click
     canvas.addEventListener('click', event => {
         var rect = canvas.getBoundingClientRect();
@@ -296,17 +239,80 @@ window.onload = function () {
         });
     });
 
-    //hex value to seperate r, g, and b method courtesy stackoverflow user Tim Down
-    //https://stackoverflow.com/a/5624139 
-    function hexToRgb(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    }
-
     //start the simulation
     startInterval(timestep);
+
+    //#region slider UI
+
+        var radSlider = document.getElementById("radius");
+        radSlider.addEventListener('input', function () {
+            radius = radSlider.value;
+        });
+    
+        var opacitySlider = document.getElementById("opacity");
+        opacitySlider.addEventListener('input', function () {
+            opacityStep = opacitySlider.value;
+        });
+    
+        var thresholdSlider = document.getElementById("threshold");
+        thresholdSlider.addEventListener('input', function () {
+            threshold = thresholdSlider.value;
+        });
+    
+        var distThresholdSlider = document.getElementById("distThreshold");
+        distThresholdSlider.addEventListener('input', function () {
+            distThreshold = distThresholdSlider.value;
+        });
+    
+    
+        timestepSlider.addEventListener('mouseup', function () {
+            timestep = timestepSlider.value;
+            clearInterval(intervalId);
+            startInterval(timestep);
+        });
+    
+        var amountBox = document.getElementById("amount");
+        amountBox.addEventListener('input', function () {
+            sessionStorage.setItem("amount", amountBox.value);
+        });
+    
+        //pause button toggle
+        var pauseToggle = false;
+        var pause = document.getElementById("pause");
+        pause.addEventListener("click", function () {
+            if (!pauseToggle) {
+                clearInterval(intervalId);
+                pauseToggle = true;
+                pause.innerHTML = "Play";
+            } else {
+                startInterval(timestepSlider.value);
+                pauseToggle = false;
+                pause.innerHTML = "Pause";
+            }
+        });
+    
+        //slider div toggle
+        var divToggle = true;
+        sliderButton.addEventListener("click", function () {
+            if (!divToggle) {
+                sliderBG.style.display = 'none';
+                sliderDiv.style.display = 'none';
+                divToggle = true;
+            }
+            else {
+                sliderBG.style.display = 'block';
+                sliderDiv.style.display = 'block';
+                divToggle = false;
+            }
+        });
+    
+        //colour picker listener
+        document.getElementById("colorPicker").addEventListener("change", function (event) {
+            var color = event.target.value;
+            colorR = hexToRgb(color).r;
+            colorG = hexToRgb(color).g;
+            colorB = hexToRgb(color).b
+        }, false);
+    
+        //#endregion slider UI
 }
